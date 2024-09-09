4876e1b597cf1b4e3f2a5b84cff540acc20b98b6
Bugra Akca <bgrakca@gmail.com>
2024-09-09 09:29:16 +0300

    Init drag and drop op

---
+++ Source/ProjectNerio/Public/SItemDrag.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/DragDropOperation.h"
+#include "SItemDrag.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTNERIO_API USItemDrag : public UDragDropOperation
+{
+	GENERATED_BODY()
+};
